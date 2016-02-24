94c03b8cfb21c009d5d4ef1b6e3b38b36938fcb6
Davidslc <djjohnson@gmail.com>
2016-02-24 16:01:06 -0700

    Refactored js file

--- purchase.js
+++ purchase.js
@@ -1,22 +1,43 @@
+var bankBalance = prompt("What is your bank balance?");
+var amount = 0;
+var phoneCount = 0;
+var accessoryCount = 0;
+
 const TAX_RATE = 0.08;
-const PHONE_PRICE = 699.95;
-const ACCESSORY_PRICE = 49.95;
+const PHONE_PRICE = 99.95;
+const ACCESSORY_PRICE = 9.95;
+const SPENDING_THRESHOLD = bankBalance * 0.75;
 
-function phonePurchase() {
-    var phoneTotal = 0;
-    var phoneToBuy = 0;
-    var willingToSpend = prompt("What is your bank balance?") * 0.50;
-    while (willingToSpend > phoneTotal) {
-        phoneToBuy += 1;
-        phoneTotal = (PHONE_PRICE + ACCESSORY_PRICE) * phoneToBuy;
 
-    }
 
-    var totalNumber = phoneToBuy - 1;
-    var totalPurchase = phoneTotal - (PHONE_PRICE + ACCESSORY_PRICE);
-    var totalWithTax = (totalPurchase * TAX_RATE) + totalPurchase;
-
-    return "You can buy " + totalNumber + " phones with accessories for $" + totalWithTax.toFixed(2);
+function calculateTax(amount) {
+    return amount * TAX_RATE;
 }
 
-console.log(phonePurchase());
+function formatAmount(amount) {
+    return "$" + amount.toFixed(2);
+}
+
+// Buy phones while you have the money in your bank account
+while (amount < SPENDING_THRESHOLD) {
+    amount += PHONE_PRICE;
+    phoneCount += 1;
+    // Is the amount over the spending threshold?
+    if (amount < SPENDING_THRESHOLD) {
+        amount += ACCESSORY_PRICE;
+        accessoryCount += 1;
+    }
+
+}
+
+
+// Add tax
+amount += calculateTax(amount);
+
+console.log("Your purchase: " + formatAmount(amount) + "\nNumber of phones: " + phoneCount + "\nNumber of accessories: " + accessoryCount);
+
+// It there enough in you bank account?
+if (amount > bankBalance) {
+    console.log("You can't afford this purchase.")
+}
+
