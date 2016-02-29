d0ad31013a951b2a6df1b4f1d2c5a595327eb14e
Davidslc <djjohnson@gmail.com>
2016-02-29 14:07:17 -0700

    Name search

---
+++ search/search.js
@@ -0,0 +1,50 @@
+var info = [
+    {
+        name: "Harry",
+        age: 26,
+        country: "US",
+        skill: "Web Development"
+    },
+    {
+        name: "Moe",
+        age: 28,
+        country: "UK",
+        skill: "Web Design"
+    },
+    {
+        name: "Curly",
+        age: 31,
+        country: "Canada",
+        skill: "Writing"
+    },
+    {
+        name: "Emily",
+        age: 24,
+        country: "US",
+        skill: "Chemical Engineering"
+    }
+];
+
+while (true) {
+    var input = prompt("Enter the name or type 'quit' to exit");
+    input = input.toLowerCase();
+
+    if (input === 'quit') {
+        document.write("<h2>Closing the document</h2>");
+        break;
+    }
+    else if (input === "") {
+        alert("Enter a valid name");
+    } else {
+        break;
+    }
+}
+    for (var i = 0; i < info.length; i++) {
+        if (info[i]['name'].toLowerCase() === input) {
+            document.write("Student: " + info[i]['name'] + '<br>');
+            document.write("Age: " + info[i]['age'] + '<br>');
+            document.write("Country: " + info[i]['country'] + '<br>');
+            document.write("Skill: " + info[i]['skill'] + '<br>');
+        }
+    }
+
